ddf66ee83e53f2256e0576e3b39793e876357f91
Roman Danylovych <roman.danylovych@epfl.ch>
2023-12-02 22:48:11 +0100

    reduce index.js and test angry blinking

--- index.js
+++ index.js
@@ -11,52 +11,8 @@ const ACTION = {
     ANGER: 3
 }
 
-
-// Turn your light on, then turn it off again after 5 seconds
-if (device) {
-    AHHH()
-
-}
-
-
-function grad() {
-    //console.log("AH")
-    /*for(let t = 0; t <= 25; t++) { 
-        for(let i = 1; i <= 7; i++) { 
-            let timeoutId = setTimeout(() => litra.setZoneColorRGB(device, i, 0, 0, (((t*10)*(i/7)) % 250 )), t*10 + i*100);
-        }
-        //clearTimeout(timeoutId)
-    }*/
-    for(let i = 1; i <= 7; i++) { 
-        let timeoutId = setTimeout(() => litra.setZoneColorRGB(device, i, i * 209 / 7, i * 170 / 7, i * 255 / 7 ), i*100);
-        setTimeout(() => litra.setZoneColorRGB(device, 8-i, i * 209 / 7, i * 170 / 7, i * 255 / 7 ), i*100 + 700);
-        //setTimeout(() => litra.setZoneColorRGB(device, i, 0, i * 170 / 7, 0), i*100 + 700);
-        //setTimeout(() => litra.setZoneColorRGB(device, i, 0, 0, i * 255 / 7 ), i*100 + 1400);
-    }
-    clearTimeout(timeoutId)
-     
-}
-
-function AHHH() {
-    for(let i = 1; i <= 10; i+=2) {
-        let timeoutId = setTimeout(() => tell_light_to(ACTION.ANGER), (i+1)*100)
-        setTimeout(() => tell_light_to(), i*100)
-    }
-}
-
-
-function tell_light_to(action) {
-    switch(action) {
-        case ACTION.MOTIVATE: setLightMood(241, 194, 50); break;
-        case ACTION.WAKE_UP: setLightMood(41, 134, 204); break;
-        case ACTION.SOOTH: setLightMood(188, 139, 145); break;
-        case ACTION.ANGER: setLightMood(0, 0, 255); break;
-        default: setLightMood(255, 255, 255)
-    }
-}
-
-function setLightMood(R, G ,B) {
-    litra.setRGBColor(device, R, G, B)
+if(device) {
+    wake_up_fade()
 }
 
 function wake_up_fade() {
