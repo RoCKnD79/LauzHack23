b9b5d71e7a1842cc30aca95428e5c9234fcdce64
Roman Danylovych <roman.danylovych@epfl.ch>
2023-12-02 22:39:56 +0100

    call js functions from python

--- index.js
+++ index.js
@@ -14,22 +14,6 @@ const ACTION = {
 
 // Turn your light on, then turn it off again after 5 seconds
 if (device) {
-    //litra.setRGBColor(device, 1, 0, 0);
-    //litra.setBrightnessPercentage(device, 10);
-
-    //setTimeout(() => litra.setRGBColor(device, 255, 0, 0), 0)
-    //setTimeout(() => litra.setRGBColor(device, 0, 255, 0), 500)
-    //setTimeout(() => litra.setRGBColor(device, 0, 0, 255), 1000)
-
-    /*for(let i = 0; i < 25; i++) { 
-        setTimeout(() => litra.setRGBColor(device, i*10, 0, 0), 100*i)
-        setTimeout(() => litra.setRGBColor(device, 0, i*10, 0), 100*i + 2500)
-        setTimeout(() => litra.setRGBColor(device, 0, 0, i*10), 100*i + 5000)
-    }*/
-
-    //let intervalId = setInterval(() => wake_up(), 7*70*2)
-    //grad()
-    //tell_light_to(ACTION.SOOTH)
     AHHH()
 
 }
--- test_jspy.py
+++ test_jspy.py
@@ -1,6 +1,36 @@
 from pythonmonkey import require as js_require
+from globals import *
 
-js_lib = js_require('./index.js')
+import os
+import sys
+
+from execjs import get
+
+runtime = get('Node')
+context = runtime.compile('''
+module.paths.push('%s');
+const litra = require('litra');
+const device = litra.findDevice();
+
+function setLightMood(R, G ,B) {
+    litra.setRGBColor(device, R, G, B);
+}
+''' % os.path.join(os.path.dirname(__file__),'node_modules'))
 
 
-js_lib.sooth()
+
+def tell_light_to(action):
+    setLightMood = 'setLightMood'
+    match action:
+        case ACTION.MOTIVATE: context.call(setLightMood, 241, 194, 50)
+        case ACTION.WAKE_UP: context.call(setLightMood, 41, 134, 204)
+        case ACTION.SOOTH: context.call(setLightMood, 188, 139, 145)
+        case ACTION.ANGER: context.call(setLightMood, 0, 0, 255)
+        case _: context.call(setLightMood, 255, 255, 255)
+
+def main():
+    tell_light_to(ACTION.WAKE_UP)
+
+
+if __name__ == "__main__":
+    main()
